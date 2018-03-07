0f651607cbcdcc174c96a7bf8bd9f3be9b826858
x0216u <940120528@qq.com>
2018-03-07 11:36:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import web
+
+urls = (
+    '/wx', 'Handle',
+)
+
+class Handle(object):
+    def GET(self):
+        return "hello, this is a test"
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
